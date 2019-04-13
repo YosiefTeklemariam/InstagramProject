3adb4d4d58784a63eefac4e418e8585eec439c4d
samarawit haile <sanigirl2005@gmail.com>
2019-04-13 11:47:01 -0500

    added reposiory

--- src/main/java/edu/mum/cs/InstagramProject/repository/UserRelationRepository.java
+++ src/main/java/edu/mum/cs/InstagramProject/repository/UserRelationRepository.java
@@ -3,7 +3,7 @@ package edu.mum.cs.InstagramProject.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import edu.mum.cs.InstagramProject.domain.UserRelation;
-
+//dellte
 public interface UserRelationRepository extends MongoRepository<UserRelation, String>  {
 
 }
